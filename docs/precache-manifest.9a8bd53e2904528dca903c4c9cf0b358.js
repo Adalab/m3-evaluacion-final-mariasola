d98ef5819170ced862ac25a6f6b120f2875f0607
Mariasola <mariasolahornedo@gmail.com>
2019-09-24 16:24:08 +0200

    build docs

---
+++ docs/precache-manifest.9a8bd53e2904528dca903c4c9cf0b358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745b1bc51c95c973437138d1176d22b1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7f11d22651a3abc9965d",
+    "url": "./static/css/main.1b5c7e63.chunk.css"
+  },
+  {
+    "revision": "e39433e132172358e285",
+    "url": "./static/js/2.80f44211.chunk.js"
+  },
+  {
+    "revision": "7f11d22651a3abc9965d",
+    "url": "./static/js/main.555d801d.chunk.js"
+  },
+  {
+    "revision": "d3970669254458402a3b",
+    "url": "./static/js/runtime-main.bdc51ec1.js"
+  },
+  {
+    "revision": "15c5ace1868a33f8b67be97a987d7964",
+    "url": "./static/media/rickandMortyLogo.15c5ace1.png"
+  }
+]);
